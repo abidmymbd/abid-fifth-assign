b11bb4b43a5a9182fdb06f8764c2c4f04b75753c
Abid Hasan <abid.mym.bd@gmail.com>
2025-08-29 19:19:21 +0600

    added full JS

--- script.js
+++ script.js
@@ -11,6 +11,73 @@ lifeHeart.forEach(heart => {
   });
 });
 
+
+///////////// Call Button Part  //////////////
+
+let callCoins = 100; 
+
+const allRemainingCoins = document.getElementById('all-rcoins');
+
+document.querySelectorAll('.btn-call').forEach(callingBtn => {
+  callingBtn.addEventListener('click', () => {
+    if (callCoins < 20) {
+      alert('Not enough coins!');
+      return;
+    }
+
+    callCoins -= 20; 
+    allRemainingCoins.textContent = callCoins;
+
+    const mainBoxCard = callingBtn.closest('.main-box');
+    const n = mainBoxCard.querySelector('span').textContent.trim();
+
+    alert(`Calling ${n}`);
+  });
+});
+
+
+// //// Call History And Clear Button //
+
+const callHistoryButtons = document.querySelectorAll('.btn-call');
+const callHistoryContainer = document.getElementById('full-history-data');
+
+callHistoryButtons.forEach(callHistoryButton => {
+    callHistoryButton.addEventListener('click', () => {
+
+        const HistoryCard = callHistoryButton.closest('.main-box'); 
+        const serviceName = HistoryCard.querySelector('h1').innerText;
+        const callNumber = HistoryCard.querySelector('span').innerText;
+        const realTime = new Date().toLocaleTimeString();
+
+        const addNewHistoryItem = document.createElement('div');
+        addNewHistoryItem.className = 'single-history';
+        addNewHistoryItem.innerHTML = `
+            <div class="flex justify-between items-center bg-[#F2F2F2] p-4 rounded-2xl mb-3">
+                <div>
+                  <h1 class="text-[18px] font-semibold">${serviceName}</h1>
+                  <p class="text-[18px] text-[#5C5C5C]">${callNumber}</p>
+                </div>
+                <div>
+                  <p>${realTime}</p>
+                </div>
+            </div>
+        `;
+
+        callHistoryContainer.appendChild(addNewHistoryItem);
+    });
+});
+
+
+// // Clear Button //
+
+
+const callClearButton = document.getElementById('clear-btn');
+
+callClearButton.addEventListener('click',function(){
+    callHistoryContainer.innerHTML = '';
+});
+
+
 // // Challenge Part - Copy Count //
 
 const copyCount = document.getElementById('copy-count')
@@ -45,29 +112,3 @@ function copyBtnCopy() {
   });
 }
 copyBtnCopy();
-
-
-///////////// Call Button Part  //////////////
-
-let callCoins = 100; 
-
-const allRemainingCoins = document.getElementById('all-rcoins');
-
-document.querySelectorAll('.btn-call').forEach(callingBtn => {
-  callingBtn.addEventListener('click', () => {
-    if (callCoins < 20) {
-      alert('Not enough coins!');
-      return;
-    }
-
-    callCoins -= 20; 
-    allRemainingCoins.textContent = callCoins;
-
-    const mainBoxCard = callingBtn.closest('.main-box');
-    const n = mainBoxCard.querySelector('span').textContent.trim();
-
-    alert(`Calling ${n}`);
-  });
-});
-
-
