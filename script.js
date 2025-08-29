78e84d434d21696f869ace0369f0f43ea729ef69
Abid Hasan <abid.mym.bd@gmail.com>
2025-08-29 07:30:01 +0600

    again js

--- script.js
+++ script.js
@@ -11,4 +11,37 @@ lifeHeart.forEach(heart => {
   });
 });
 
+// // Challenge Part - Copy Count //
 
+const copyCount = document.getElementById('copy-count')
+const copybtn = document.querySelectorAll('.btn-copy')
+
+let countOfCopy = 2
+
+copybtn.forEach(copy =>{
+    copy.addEventListener('click', () =>{
+        countOfCopy++;
+        copyCount.textContent = countOfCopy;
+    })
+})
+
+// // Copy & Paste //
+
+function copyBtnCopy() {
+  const copyBtns = document.querySelectorAll('.btn-copy');
+
+  copyBtns.forEach(cBtn => {
+    cBtn.addEventListener('click', () => {
+
+      const mainBox = cBtn.closest('.main-box');
+      const numberElement = mainBox.querySelector('span');
+      const number = numberElement.textContent.trim();
+
+      navigator.clipboard.writeText(number)
+        .then(() => {
+          alert(`${number} is copied`);
+        })
+    });
+  });
+}
+copyBtnCopy();
