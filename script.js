d884aafe18071c12c85050f6d053574d2166eba8
Abid Hasan <abid.mym.bd@gmail.com>
2025-08-29 07:24:32 +0600

    added js fot hreart

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// //
+// // Heart Count //
+
+const heartCount = document.getElementById('heart-count')
+const lifeHeart = document.querySelectorAll('.life-heart')
+let countOfHeart = 0
+
+lifeHeart.forEach(heart => {
+  heart.addEventListener('click', () => {
+    countOfHeart++;
+    heartCount.textContent = countOfHeart;
+  });
+});
+
+
